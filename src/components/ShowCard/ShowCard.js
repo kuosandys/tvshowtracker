bd6dc38c6fc09b501fb9bb5ba822cf2860ab6258
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-15 19:53:00 +0100

    Handle errors thrown by accessing null-ish API show object properties by adding conditional operators and providing alt values"

--- src/components/ShowCard/ShowCard.js
+++ src/components/ShowCard/ShowCard.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
-import ShowDetails from "../ShowDetails/ShowDetails";
+import { Link, useRouteMatch } from "react-router-dom";
 
 // Displays show overview info: name, poster, premiered/s, status, country, button to add to watch list
 function ShowCard(props) {
@@ -11,9 +10,9 @@ function ShowCard(props) {
     <Link to={`${match.url}/${show.id}`}>
       <div>
         <h2>{show.name}</h2>
-        <img src={show.image.medium} alt="TV show poster" />
-        <h3>Country: {show.network.country.code}</h3>
-        <h3>{`Premiered: ${show.premiered.slice(0, 4)}`}</h3>
+        <img src={show.image?.medium} alt="TV show poster" />
+        <h3>Country: {show.network?.country?.code || "Unknown"}</h3>
+        <h3>{`Premiered: ${show.premiered?.slice(0, 4) || "Unknown"}`}</h3>
         <h3>{`Status: ${show.status}`}</h3>
       </div>
     </Link>
