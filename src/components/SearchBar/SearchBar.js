0b906a18f78227c9666c68e094c78dd23016d467
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-16 16:21:34 +0100

    Add styles for SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,9 +9,21 @@ function SearchBar(props) {
   };
 
   return (
-    <form onSubmit={(e) => handleSubmitSearch(e, searchInput)}>
-      <input type="text" onChange={handleChange}></input>
-      <button type="submit">Search</button>
+    <form
+      onSubmit={(e) => handleSubmitSearch(e, searchInput)}
+      className="flex items-stretch justify-center"
+    >
+      <input
+        type="text"
+        onChange={handleChange}
+        className="mx-3 px-3 rounded"
+      ></input>
+      <button
+        type="submit"
+        className="bg-blue-400 hover:bg-blue-700 rounded px-3 py-1.5 text-white mx-3"
+      >
+        Search
+      </button>
     </form>
   );
 }
