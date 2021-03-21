29d35da16a6294a123e044857e5aaa276bb6f6ef
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-21 13:36:38 +0100

    Add feature to filter episodes by season

--- src/components/EpisodesWrapper/EpisodesWrapper.js
+++ src/components/EpisodesWrapper/EpisodesWrapper.js
@@ -3,50 +3,87 @@ import React, { useState, useEffect } from "react";
 import EpisodeCard from "../EpisodeCard/EpisodeCard";
 import WatchedButton from "../WatchedButton/WatchedButton";
 
-function EpisodesWrapper({showId, timezone, watchedEpisodes, setWatchedEpisodes}) {
+function EpisodesWrapper({
+  showId,
+  timezone,
+  watchedEpisodes,
+  setWatchedEpisodes,
+}) {
   const [episodesData, setEpisodesData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [seasons, setSeasons] = useState([]);
+  const [seasonSelected, setSeasonSelected] = useState(1);
 
   useEffect(() => {
-    const fetchShowData = async () => {
+    const fetchEpisodesData = async () => {
       try {
+        // Fetch episodes data from TVmaze API
         let response = await fetch(
           `http://api.tvmaze.com/shows/${showId}/episodes?specials=1`
         );
         let dataObject = await response.json();
+
+        // Flatten dataObject into array; set Episodes data
         let dataArray = Object.values(dataObject);
         setEpisodesData(dataArray);
+
+        // Get all distinct seasons as an array; set Seasons
+        let seasonsArray = [
+          ...new Set(dataArray.map((episode) => episode.season)),
+        ];
+        setSeasons(seasonsArray);
+
         setIsLoaded(true);
       } catch (error) {
         alert(error);
       }
     };
-    fetchShowData();
+    fetchEpisodesData();
   }, [showId]);
 
-  if (isLoaded) {
-    return (
-      <div className="max-w-screen-md mx-auto">
-        {episodesData.map((episode) => {
-          return (
-            <EpisodeCard key={episode.id} episode={episode} timezone={timezone}>
-              <WatchedButton
-                episodeId={episode.id}
-                watched={watchedEpisodes.includes(episode.id) ? true : false}
-                handleWatch={setWatchedEpisodes}
-              ></WatchedButton>
-            </EpisodeCard>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <p>loading</p>
-      </div>
-    );
-  }
+  const seasonSelectedClass = "font-bold";
+
+  return (
+    <div className="max-w-screen-md mx-auto">
+      {isLoaded && (
+        <div>
+          <ul className="flex border-2 overflow-x-scroll">
+            {seasons.map((season) => {
+              return (
+                <li
+                  onClick={() => setSeasonSelected(season)}
+                  className={`${
+                    season === seasonSelected && seasonSelectedClass
+                  } px-3 py-2`}
+                >
+                  Season {season}
+                </li>
+              );
+            })}
+          </ul>
+          {episodesData
+            .filter((episode) => episode.season === seasonSelected)
+            .map((episode) => {
+              return (
+                <EpisodeCard
+                  key={episode.id}
+                  episode={episode}
+                  timezone={timezone}
+                >
+                  <WatchedButton
+                    episodeId={episode.id}
+                    watched={
+                      watchedEpisodes.includes(episode.id) ? true : false
+                    }
+                    handleWatch={setWatchedEpisodes}
+                  ></WatchedButton>
+                </EpisodeCard>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default EpisodesWrapper;
