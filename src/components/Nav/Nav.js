884845bb71c79ecaa49af76d8732a87b54fe0b9f
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-28 14:41:41 +0200

    Import User Context to Nav bar and display SignIn/Up or SignOut buttons conditionally

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import { auth } from "../../firebase/firebaseIndex";
+
+import { UserContext } from "../ContextProviders/UserContextProvider";
 
 function Nav({ children }) {
+  const user = useContext(UserContext);
   return (
     <div className="fixed top-0 left-0 right-0 py-1 flex items-center justify-between bg-indigo-900 text-white">
       <ul className="flex h-12 items-center">
@@ -9,8 +13,13 @@ function Nav({ children }) {
           <Link to="/">Home</Link>
         </li>
         <li className="mx-5 font-bold hover:underline">
-          <Link to="/shows">My Shows</Link>
+          <Link to="/shows">{user ? `${user.displayName}'s` : "My"} Shows</Link>
         </li>
+        {user ? (
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        ) : (
+          <Link to="sign-up">Sign Up</Link>
+        )}
       </ul>
       {children}
     </div>
