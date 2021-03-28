6c299a08fd5cbe20d1992de1f7cee339d8d38ad3
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-28 14:42:20 +0200

    Import User Context and Provider, create Routes for sign-in and sign-up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,12 @@ import Search from "../Search/Search";
 import Nav from "../Nav/Nav";
 import User from "../User/User";
 import SearchBar from "../SearchBar/SearchBar";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
+
 import { useSessionState, arrayDataReducer } from "../../helpers/helpers";
-import { UserContext, TrackedShowsContext } from "../ContextProviders/Contexts";
+import { TrackedShowsContext } from "../ContextProviders/Contexts";
+import { UserContextProvider } from "../ContextProviders/UserContextProvider";
 
 function App() {
   const [trackedShows, setTrackedShows] = useSessionState(
@@ -17,7 +21,6 @@ function App() {
 
   const [searchQuery, setSearchQuery] = useState("");
   const [searchRequested, setSearchRequested] = useState(false);
-  const user = null;
 
   const handleSubmitSearch = (e, currentSearchInput) => {
     e.preventDefault();
@@ -26,23 +29,31 @@ function App() {
   };
 
   return (
-    <div className="px-10 pt-10 font-sans font-extralight bg-gray-100 h-full min-h-screen text-lg">
-      <Nav user={user}>
-        <SearchBar handleSubmitSearch={handleSubmitSearch} />
-      </Nav>
-      <TrackedShowsContext.Provider value={{ trackedShows, setTrackedShows }}>
-        {searchRequested && <Redirect to="/search" />}
-        <User />
-        <Switch>
-          <Route path="/search">
-            <Search
-              searchQuery={searchQuery}
-              setSearchRequested={setSearchRequested}
-            />
-          </Route>
-        </Switch>
-      </TrackedShowsContext.Provider>
-    </div>
+    <UserContextProvider>
+      <div className="px-10 pt-10 font-sans font-extralight bg-gray-100 h-full min-h-screen text-lg">
+        <Nav>
+          <SearchBar handleSubmitSearch={handleSubmitSearch} />
+        </Nav>
+        <TrackedShowsContext.Provider value={{ trackedShows, setTrackedShows }}>
+          {searchRequested && <Redirect to="/search" />}
+          <User />
+          <Switch>
+            <Route path="/search">
+              <Search
+                searchQuery={searchQuery}
+                setSearchRequested={setSearchRequested}
+              />
+            </Route>
+            <Route path="/sign-up">
+              <SignUp />
+            </Route>
+            <Route path="/sign-in">
+              <SignIn />
+            </Route>
+          </Switch>
+        </TrackedShowsContext.Provider>
+      </div>
+    </UserContextProvider>
   );
 }
 
