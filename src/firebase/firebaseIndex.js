e29b2ce0860b3f59d30bc66c46a311aaa48be361
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-03-28 14:40:47 +0200

    Initialize app and export auth and firestore objects, signInWithGoogle function

--- src/firebase/firebaseIndex.js
+++ src/firebase/firebaseIndex.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
@@ -9,4 +13,16 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-export default firebaseConfig;
+// Init firebase app
+firebase.initializeApp(firebaseConfig);
+
+// auth
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
+
+const firestore = firebase.firestore();
+
+export { auth, firestore, signInWithGoogle };
